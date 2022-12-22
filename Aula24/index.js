175b1f4ea7786927a6a6c741a44fc5064ea63711
Will Oliver <studioswilloliver@gmail.com>
2022-12-22 17:29:39 -0300

    criação de modelo para atividades futuras

---
+++ Aula24/index.js
@@ -0,0 +1,17 @@
+let hora = 19;
+
+if(hora <= 12){
+    console.log('Bom dia')
+}else if(hora > 12 && hora <= 17) {
+    console.log('Boa tarde')
+} else {
+    console.log('Boa noite')
+} 
+
+let tenhoGrana = true
+
+if (tenhoGrana = true){
+    console.log('Vou sair')
+}else{
+    console.log('Não vou sair')
+}
---
+++ Aula25/index.js
@@ -0,0 +1,14 @@
+const numero = 10
+ 
+// Se (numero >= 0 && numero <= 5) ocorrer, faca isso (codigo)
+// Se não faca isso (o codigo)
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O numero está entre 0 e 5.')
+} else if(numero >= 6 && numero <= 8){
+    console.log('O numero está entre 6 e 8.')
+} else if(numero >= 9 && numero <= 11){
+    console.log('O numero está entre 9 e 11.')
+} else {
+    console.log('O numero não está entre 0 e 11')
+}
