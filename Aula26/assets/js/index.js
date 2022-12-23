8ce5b2c538cfa1bea33a91ee654bc7c59ee25b5f
Will Oliver <studioswilloliver@gmail.com>
2022-12-23 11:41:36 -0300

    mais tentativas

--- Aula26/assets/js/index.js
+++ Aula26/assets/js/index.js
@@ -13,20 +13,17 @@ function meuEscopo() {
         }else{
            imc = peso.value / (altura.value * altura.value)
         }
-
-        
-        
         
 
         if (imc < 18.5) {
-            res.textContent += `Seu IMC é ${imc.toFixed(2)} (Abaixo do peso)`    
-        } else if (imc >= 18.5 && imc <= 24.9){
+            res.textContent += `Seu IMC é ${imc.toFixed(2)} (Abaixo do peso)`
+        } else if (imc >= 18.5 && imc <= 24.99){
             res.textContent += `Seu IMC é ${imc.toFixed(2)} (Peso normal)` 
-        } else if (imc >= 25 && imc <= 29.9){
+        } else if (imc >= 25 && imc <= 29.99){
             res.textContent += `Seu IMC é ${imc.toFixed(2)} (Sobrepeso)` 
-        } else if (imc > 30 && imc < 34.9){
+        } else if (imc > 30 && imc < 34.99){
             res.textContent += `Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)` 
-        } else if (imc > 35 && imc < 39.9){
+        } else if (imc > 35 && imc < 39.99){
             res.textContent += `Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)` 
         } else{
             res.textContent += `Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)`
