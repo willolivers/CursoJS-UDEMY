7bbaad746110580a1f38c433790f872a7b4f73a9
Will Oliver <studioswilloliver@gmail.com>
2022-12-26 17:20:20 -0300

    aula 27, 28 e 29

---
+++ Aula27/index.js
@@ -0,0 +1,17 @@
+//(CONDICAO) ? 'VALOR PARA VERDADEIRO' : 'VALOR PARA FALSO'
+
+const pontuacaoUsuario = 1000;
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+
+
+console.log(nivelUsuario)
+
+/*
+if (pontuacaoUsuario >= 1000) {
+    console.log('Usuario VIP')
+} else {
+    console.log('Usuario normal')
+}
+*/
---
+++ Aula29/index.js
@@ -0,0 +1,41 @@
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo'
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda'
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terca'
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta'
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta'
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta'
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado'
+            return diaSemanaTexto;
+    
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+
+    
+}
+
+const data = new Date()
+let diaSemana = data.getDay()
+const diaSemanaTexto = getDiaSemanaTexto()
+
+
+
+console.log(diaSemana, diaSemanaTexto)
