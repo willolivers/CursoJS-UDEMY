284b23af2e3a1d89282b49145b91123dfd3580f3
Will Oliver <studioswilloliver@gmail.com>
2023-04-10 22:01:05 -0300

    aula

---
+++ Aula62/index.js
@@ -0,0 +1,7 @@
+//const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+//const novo = nomes.slice(0, -1)
+
+//const nome = 'Jessé Willy da Silva Oliveira'
+const nomes = [ 'Jessé', 'Willy', 'da', 'Silva', 'Oliveira' ]
+const nome = nomes.join(' ')
+console.log(nome);
