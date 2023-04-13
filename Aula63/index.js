7e6243d23c5941f973a1e0ba8889127e842b38a6
Will Oliver <studioswilloliver@gmail.com>
2023-04-12 21:29:45 -0300

    aulas sobre splice e concat

---
+++ Aula63/index.js
@@ -0,0 +1,11 @@
+const nomes = ['Maria', 'João', 'Jessé', 'Will'];
+
+//nomes.splice(indice, delete, elem1, elem2, elem3);
+//pop
+//const removidos = nomes.splice(4, 1);
+//shift
+//const removidos = nomes.splice(0, 1)
+//push
+nomes.splice(nomes.length, 0, 'Luiz', 'Will')
+console.log(nomes)
+
---
+++ Aula64/indes.js
@@ -0,0 +1,4 @@
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+const num3 = num1.concat(num2, [7, 8, 9], 'Jessé');
+console.log(num3)
