db3d952c73e60bdb44bcdb3719c2d0faea18ed35
Will Oliver <80014945+willolivers@users.noreply.github.com>
2023-05-01 21:15:44 -0300

    defineProperty

--- Aula71/index.js
+++ Aula71/index.js
@@ -1,10 +1,25 @@
 function Produto(nome, preco, estoque){
-    this.nome = nome;
-    this.preco = preco;
-    this.estoque = estoque;
 
-    Object.defineProperties(this, 'estoque', {
-        enumerable: true,
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave
+        value: estoque, // valor
+        writable: false, // pode alterar
+        configurable: true // configuravel se for true
+    });
+
+    Object.defineProperties(this, {
+        nome: {        
+            enumerable: true, // mostra a chave
+            value: nome, // valor
+            writable: true, // pode alterar
+            configurable: true // configuravel se for true
+        },
+        preco: {
+            enumerable: true, // mostra a chave
+            value: preco, // valor
+            writable: true, // pode alterar
+            configurable: true // configuravel se for true
+        }
     })
 }
 
