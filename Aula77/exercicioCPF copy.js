f7004f538c8347fc3274ee926306a3abbd4ca577
Will Oliver <80014945+willolivers@users.noreply.github.com>
2023-05-19 14:37:37 -0300

    exercicio cpfValid

---
+++ Aula77/exercicioCPF copy.js
@@ -0,0 +1,18 @@
+function ValidaCpf (cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo',{
+        enumerable: true,
+        get: function () {
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+};
+
+ValidaCpf.prototype.verificaCpf = function () {
+    if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false
+    return true;
+};
+
+const cpf = new ValidaCpf('619.373.923-84');
+console.log(cpf.cpfLimpo)
+console.log()
--- Aula77/exercicioCPF.js
+++ Aula77/exercicioCPF.js
@@ -22,23 +22,40 @@ function ValidaCpf(cpfEnviado) {
 ValidaCpf.prototype.valida = function () {
     if (typeof this.cpfLimpo === 'underfined') return false;
     if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
-    return true;
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
 }
 
 ValidaCpf.prototype.criaDigito = function(cpfParcial){
     const cpfArray = Array.from(cpfParcial)
     
     let regressivo = cpfArray.length + 1;
-    const digito = cpfArray.reduce((ac, val) => {
-        console.log(regressivo, val, regressivo * val)
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
         regressivo--;
         return ac;
     }, 0)
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
 }
 
-const cpf = new ValidaCpf('705.484.450-52');
-console.log(cpf.cpfLimpo);
-console.log(cpf.valida());
+ValidaCpf.prototype.isSequencia = function () {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidaCpf('070.987.720-03');
+
+
+if(cpf.valida()){
+    console.log('Cpf válido');
+} else {
+    console.log('Cpf inálido');
+}
