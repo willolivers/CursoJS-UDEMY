0772338ba0c2ade81a0a944f7f170ec82a68b499
Will Oliver <studioswilloliver@gmail.com>
2023-07-04 18:00:30 -0300

    aula 82

--- Aula82/herancacomclasse.js
+++ Aula82/herancacomclasse.js
@@ -1,5 +1,5 @@
 
-class DispositipoEletronico{
+class DispositipoEletronico {
     constructor(nome){
         this.nome = nome;
         this.ligado = false;
@@ -24,7 +24,28 @@ class DispositipoEletronico{
     }
 }
 
-class Smartphone extends DispositipoEletronico{}
+class Smartphone extends DispositipoEletronico{
+    constructor(nome, cor, modelo) {
+        super(nome);
 
-const s1 = new Smartphone('Iphone')
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+class Tablet extends DispositipoEletronico{
+    constructor(nome, temWifi){
+        super(nome);
+        this.temWifi = temWifi;
+    }
+
+    ligar() {
+        console.log('Olha, você alterou o método ligar.')
+    }
+}
+
+const s1 = new Smartphone('Samsung', 'Preto', 'Galaxy S10');
 console.log(s1)
+
+const t1 = new Tablet('Ipad', true);
+t1.ligar()
